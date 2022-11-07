7657d46f14537e4be7fd3a62867afbab0c588e90
Kouya96 <115438322+Kouya96@users.noreply.github.com>
2022-11-07 04:52:05 +0000

    [start]3-2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,13 @@
+class ListsController < ApplicationController
+  def new
+  end
+
+  def inndex
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+   config.hosts << "444cdac2b2df44a69bbae8eb27d1333a.vfs.cloud9.us-east-1.amazonaws.com"
 end
